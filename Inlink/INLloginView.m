9a15d4886fe8d3030c2f588feb1debe03fc73e32
Lillian Choung <lchoung@fb.com>
2014-07-08 10:47:53 -0700

    Added user sign up and log in interface

---
+++ Inlink/INLloginView.m
@@ -0,0 +1,31 @@
+//
+//  INLloginView.m
+//  Inlink
+//
+//  Created by Lillian Choung on 7/8/14.
+//  Copyright (c) 2014 Inlink. All rights reserved.
+//
+
+#import "INLloginView.h"
+
+@implementation INLloginView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Inlink/INLsignUpView.m
@@ -0,0 +1,31 @@
+//
+//  INLsignUpView.m
+//  Inlink
+//
+//  Created by Lillian Choung on 7/8/14.
+//  Copyright (c) 2014 Inlink. All rights reserved.
+//
+
+#import "INLsignUpView.h"
+
+@implementation INLsignUpView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
